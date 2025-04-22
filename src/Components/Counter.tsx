63728b7eb911a9b0b668a1491e057bb66b5e42a9
pavantechie1862 <pavanmarapalli171862@gmail.com>
2025-04-22 15:24:21 +0530

    Basic Zustand App

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,19 @@
+// src/components/Counter.tsx
+import React from "react";
+import useCounterStore from "../Store/counterStore";
+
+const Counter = () => {
+  const count = useCounterStore((state) => state.count);
+  const increase = useCounterStore((state) => state.increase);
+  const decrease = useCounterStore((state) => state.decrease);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Count: {count}</h2>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  );
+};
+
+export default Counter;
